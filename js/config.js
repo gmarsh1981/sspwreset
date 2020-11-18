454be41b678292ab0149e225b930566705d0a338
Marsh <Greg.Marsh@sony.com>
2020-11-18 15:25:27 -0800

    update config

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window._config = {
     api: {
-        invokeUrl: 'https://cv5yg5kpu0.execute-api.us-west-2.amazonaws.com/default/navcorinfs-sspw-writeToken' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cv5yg5kpu0.execute-api.us-west-2.amazonaws.com/default/navcorinfs-sspw-writeToken'  // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
