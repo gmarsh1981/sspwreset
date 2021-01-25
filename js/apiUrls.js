caa48f791c986b53e63e64f229513077a829744e
gmarsh <greg.marsh@sony.com>
2021-01-25 09:57:52 -0800

    updated api urls

--- js/apiUrls.js
+++ js/apiUrls.js
@@ -1,3 +1,3 @@
-var reset_api = 'https://uus9yip2hd.execute-api.us-west-2.amazonaws.com/sspw-reset/sspw-resetPassword'
-var create_token_api = 'https://7y1nzwe4je.execute-api.us-west-2.amazonaws.com/sspw-create/sspw-token-creation'
+var reset_api = 'https://5qtllnxd56.execute-api.us-west-2.amazonaws.com/sspw-reset/sspw-resetPassword'
+var create_token_api = 'https://jfuadzatgd.execute-api.us-west-2.amazonaws.com/sspw-create/sspw-token-creation'
 
