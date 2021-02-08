cd14cca13e3f041cf5b305138e368db94810dfac
gmarsh <greg.marsh@sony.com>
2021-02-08 10:03:36 -0800

    updated apis

--- js/apiUrls.js
+++ js/apiUrls.js
@@ -1,3 +1,3 @@
-var reset_api = 'https://41dhecsce1.execute-api.us-west-2.amazonaws.com/sspw-reset/sspw-resetPassword'
-var create_token_api = 'https://ka9ny1xnsg.execute-api.us-west-2.amazonaws.com/sspw-create/sspw-token-creation'
+var reset_api = 'https://jq35jlq3xi.execute-api.us-west-2.amazonaws.com/sspw-reset/sspw-resetPassword'
+var create_token_api = 'https://os25yicm4j.execute-api.us-west-2.amazonaws.com/sspw-create/sspw-token-creation'
 
