c3e18296f8433aea5e0a828c76620c3aad7cd144
gmarsh <greg.marsh@sony.com>
2021-02-10 14:10:56 -0800

    updated js

--- js/scripts.js
+++ js/scripts.js
@@ -21,6 +21,7 @@ function redirectorWriteToken() {
             }else{
                 document.getElementById("result").innerHTML = "<span style='color: red;'>Reset Timed Out. Refresh page and please try again.</span>"
             }
+        }
         
         if (this.responseText.includes("will be receiving an email with a link to retrieve temp password")){
             document.getElementById("result").innerHTML = "<span style='color: green;'>Information has been successfully submitted.</span>"
@@ -28,6 +29,7 @@ function redirectorWriteToken() {
         else {
             document.getElementById("result").innerHTML = "<span style='color: red;font-weight: bold;'>Unable to create password reset request. Please check username and email and try again. If information is correct please contact NOC.</span>"
         }
+    }
 
     // function to handle error
     function error(err) {
